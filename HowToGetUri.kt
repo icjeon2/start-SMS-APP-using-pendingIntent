90a524e84158c29894f2ced17867c39404c27753
incheol <101618707+icjeon2@users.noreply.github.com>
2024-04-12 15:21:18 +0900

    Create HowToGetUri.kt

---
+++ HowToGetUri.kt
@@ -0,0 +1,29 @@
+/**
+ * return uri using filePath
+ * 
+ */
+
+fun getUriFromPath(context : Context, filePath: String): Uri? {
+    val cursor: Cursor = context.contentResolver.query(
+        Media.EXTERNAL_CONTENT_URI,
+        null, "_data = '$filePath'", null, null
+    ) ?: return null
+
+    if(cursor.columnCount == 0 ||
+        cursor.count == 0)
+        return null
+
+    cursor.moveToNext()
+    val id = try {
+          cursor.getInt(cursor.getColumnIndex("_id"))
+        }catch (e: Exception){
+            Timber.e(e)
+            null
+        }finally {
+            cursor.close()
+        } ?: return null
+
+    return ContentUris.withAppendedId(
+        Media.EXTERNAL_CONTENT_URI,
+        id.toLong())
+}
